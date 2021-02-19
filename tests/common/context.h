015058b62f2bdd6c7e1641191754932cab97ecf6
Sukchan Lee <acetcom@gmail.com>
2021-02-19 12:42:49 -0500

    update it

--- tests/common/context.h
+++ tests/common/context.h
@@ -489,6 +489,7 @@ int test_db_insert_ue(test_ue_t *test_ue, bson_t *doc);
 int test_db_remove_ue(test_ue_t *test_ue);
 
 bson_t *test_db_new_simple(test_ue_t *test_ue);
+bson_t *test_db_new_multiple_qos_flow(test_ue_t *test_ue);
 
 #ifdef __cplusplus
 }
