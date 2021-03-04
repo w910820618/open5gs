2630a6f5be168d81e7c4b5432c0404daa77485d5
Sukchan Lee <acetcom@gmail.com>
2021-03-05 05:27:01 +0900

    udpate it

--- lib/sbi/ogs-sbi.h
+++ lib/sbi/ogs-sbi.h
@@ -65,6 +65,7 @@
 #include "model/sm_policy_context_data.h"
 #include "model/sm_policy_decision.h"
 #include "model/sm_policy_data.h"
+#include "model/slice_info_for_pdu_session.h"
 
 #include "custom/links.h"
 
